74e9db8351cb173a5ff4a05149952cc385f77d91
林凯丰 <1656399881@qq.com>
2022-06-26 20:50:55 +0800

    feat(user): user

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,14 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 const publicRoutes = [
   {
-    path: '/login',
+    path: '/login', // 登录页面
     name: 'login',
     component: () => import('../views/login')
+  },
+  {
+    path: '/user', // 用户页面
+    name: 'user',
+    component: () => import('../views/users/user')
   }
 ]
 
